0be9393cf6ef745ca30fb26556851ecf3578cccb
tutrilla <145156699+tutrilla@users.noreply.github.com>
2024-01-27 16:44:26 -0700

    Resized the google Map

--- aspotforme/src/app/rentparking/map/map.tsx
+++ aspotforme/src/app/rentparking/map/map.tsx
@@ -5,8 +5,8 @@ const Map: React.FC = () => {
       <div>
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12181.283709653006!2d-111.65414825!3d40.2463985!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x874d90bb13517f57%3A0x2466f47e7c66c579!2sBrigham%20Young%20University%20Museum%20of%20Art%20(MOA)!5e0!3m2!1sen!2sus!4v1706393800166!5m2!1sen!2sus"
-          width="800"
-          height="600"
+          width="600"
+          height="400"
           style={{ border: "0" }}
           allowFullScreen={true}
           loading="lazy"
